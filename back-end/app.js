79a85d30a29087881a10482844e9530e11692ed3
Stefan <lazeastefan@gmail.com>
2019-12-01 00:56:00 +0200

    final register method + login method with cookie

--- back-end/app.js
+++ back-end/app.js
@@ -2,15 +2,19 @@ const express = require('express');
 const model = require('./models');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
 const PORT = 3000;
 
 //initializare express
 const app = express();
 
+dotenv.config();
 //for json requests
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //sincronizare baza de date, in functie de modele
 model.sequelize.sync();
--- back-end/controllers/users.js
+++ back-end/controllers/users.js
@@ -5,16 +5,54 @@ const jwt = require('jsonwebtoken');
 
 const registerUser = async (req, res) => {
     let userFound = await findUserByUsername(req.body.username);
-
+    //todo validation for username + password;
     if (userFound) {
         return res.status(409).send({ message: "User already exists" });
     }
 
-    res.status(200).send({ message: "testam" })
+    const salt = bcrypt.genSaltSync(10);
+    //fails if the password attribute is not found in body
+    ePassword = bcrypt.hashSync(req.body.password, salt);
+
+    try {
+        Users.create({
+            username: req.body.username,
+            password: ePassword
+        });
+    } catch (err) {
+        return res.send(err);
+    }
+
+    res.status(201).send({ message: `User created` });
 };
 
 const login = async (req, res) => {
+    let userFound = await findUserByUsername(req.body.username);
 
+    if (!userFound) {
+        return res.status(404)
+            .send({ message: "No email related to an accout was found" });
+    }
+
+    const validPass = bcrypt.compareSync(req.body.password, userFound.password);
+    if (!validPass) {
+        return res.status(400).send({ message: "Wrong password" });
+    }
+
+    const token = jwt.sign({ id: userFound.id }, process.env.TOKEN_SECRET,
+        {
+            expiresIn: "30s"
+        });
+
+    const refreshToken = jwt.sign({ id: userFound.id }, process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "3 days" }
+    );
+
+    res.cookie("refreshToken", refreshToken, { signed: true, httpOnly: true })
+        .send({
+            token: "Bearer " + token,
+            registerUser: "Bearer " + refreshToken
+        })
 };
 
 const logout = async (req, res) => {

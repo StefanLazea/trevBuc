1d602c3023e757f60b499c88ee6f8c0a27468b28
Laurentiu Istrate <laur.constantin22@gmail.com>
2019-12-05 07:15:54 +0000

    am adaugat ruta pentru update-ul unui review

--- back-end/controllers/reviews.js
+++ back-end/controllers/reviews.js
@@ -1,10 +1,10 @@
-const Reviews = require('../models').reviews;
+const Reviews = require('../models').Reviews;
 
 const getAllReviews = async (req, res) => {
 
-    let reviewsFound;
+    //let reviewsFound;
     try{
-    await Reviews.findAll().then((allReviews) => reviewsFound = allReviews);
+    await Reviews.findAll().then((allReviews) => res.send(allReviews));
    }
    catch(err)
    {
@@ -12,9 +12,23 @@ const getAllReviews = async (req, res) => {
    }
     
 
-    res.send(reviewsFound);
+    //res.send(reviewsFound);
 };
 
+const updateReview = async (req,res) => {
+   
+   await Reviews.update( {leaving_point: req.body.leaving_point,
+        arriving_point: req.body.arriving_point,
+        leaving_hour: req.body.leaving_hour,
+        duration: req.body.duration,
+        observations: req.body.observations,
+        rating: req.body.rating,
+        congestion_level: req.body.congestion_level
+    },{where: {id: req.params.id}}).then(updatedReview => res.send(updatedReview));
+    
+}
+
 module.exports = {
-    getAllReviews
+    getAllReviews,
+    updateReview
 }
--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -11,15 +11,26 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 //route for transport types 
 router.get('/gatt', transportTypeController.getAllTransportTypes);
-//route for reviews
-router.get('/gar', reviewsController.getAllReviews);
+//route by mihnea for reviews
+router.get('/reviews',verifyAuth, reviewsController.getAllReviews);
+
+//update a review
+router.put('/reviews/:id', verifyAuth, reviewsController.updateReview);
 
 
 //todo
+// am modificat putin post-ul lui stefan
 router.post('/reviews', verifyAuth, (req, res) => {
     const leavingPoint = req.body.leaving_point;
-    Review.create({
-        leaving_point: req.body.leaving_point
+   Review.create({
+        leaving_point: req.body.leaving_point,
+        arriving_point: req.body.arriving_point,
+        leaving_hour: req.body.leaving_hour,
+        duration: req.body.duration,
+        observations: req.body.observations,
+        rating: req.body.rating,
+        congestion_level: req.body.congestion_level
+        
     })
     return res.send(`Am primit ${req.body.leaving_point}`);
 

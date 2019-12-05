f56cdcec7c4bb1221f0c641081c34da3ba4cd4e4
Laurentiu Istrate <laur.constantin22@gmail.com>
2019-12-05 11:42:47 +0000

    am adaugat relatiile intre tabele

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -5,6 +5,9 @@ const transportType = sequelize.import('./transportType.js');
 
 const Reviews = sequelize.import('./reviews.js');
 
+Users.hasMany(Reviews, {onDelete: 'cascade'});
+Reviews.hasOne(transportType, {onDelete: 'cascade'});
+
 module.exports={
     sequelize,
     Users,

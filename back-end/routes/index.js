dc51634a6fd41af9890a41d2672d1d32aeac75ee
Lazea Stefan <lazeastefan@gmail.com>
2023-01-19 17:45:13 +0200

    Update index.js

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -5,10 +5,10 @@ const reviewsController = require('../controllers/reviews')
 const verifyAuth = require('../controllers/middlewares').verifyToken;
 const Review = require("../models").Reviews;
 
+// LOGIN 
 router.post('/register', userController.registerUser);
 //route for login an user with credentials
 router.post('/login', userController.login);
-
 router.put('/resetpassword', userController.resetPassword);
 
 // REVIEWS
@@ -16,35 +16,25 @@ router.put('/resetpassword', userController.resetPassword);
 //post method
 //create a new review given the set of date from the request
 router.post('/reviews', verifyAuth, reviewsController.createReview);
-
 //get all reviews
 router.get('/reviews', reviewsController.getAllReviews);
-
 //update a review
 router.put('/reviews/:id', verifyAuth, reviewsController.updateReview);
-
 //get a user reviews
 router.get('/reviews/user/:id', reviewsController.getReviewsByUserId);
-
 //get review for the transport type with the id
 router.get('/reviews/transport-type/:id', reviewsController.getReviewsByTransportTypeId);
-
 //delete review by id
 router.delete('/reviews/:id', reviewsController.deleteReviewById);
-
 //get review by id
 router.get('/reviews/:id', reviewsController.getReviewById);
 
 // TRANSPORT-TYPE
-
 //get all transport types
 router.get('/transport-type', transportTypeController.getAllTransportTypes);
 //create a transport type
 router.post('/transport-type', transportTypeController.createTransportType);
-
-
 router.get('/transport-type/:id', transportTypeController.getTranportTypeById);
 router.get('/transport-type/type/:type', transportTypeController.getAllTranportTypesByType);
-
 
 module.exports = router;
